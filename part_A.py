85f56d479988e1b05514e3b198ff67e4631f295a
Ole-Christian Kran <Ole-Christian.Kran@student.nhh.no>
2024-10-02 12:49:23 +0200

    Implement part A

--- part_A.py
+++ part_A.py
@@ -1,35 +1,91 @@
+# -*- coding: utf-8 -*-
 """
-TECH2 mandatory assignment - Part A
+Created on Tue Oct  1 13:10:54 2024
 
-Write the implementation of part A of the exercise below.
+@author: olech
 """
+# Method 1 using for loops
+import math
+
+num_list = [1,2,3,4,5]
 
 def std_loops(x):
-    """
-    Compute standard deviation of x using loops.
-
-    Parameters
-    ----------
-    x: Sequence of numbers
-
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
-
-def std_builtin(x):
-    """
-    Compute standard deviation of x using the built-in functions sum()
-    and len().
-
-    Parameters
-    ----------
-    x: Sequence of numbers
-
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
+    sum_x = 0
+    N = 0
     
+    for i in num_list:
+        N +=1
+    
+    #computing the mean
+        
+    for i in range(N):
+        sum_x = sum_x + x[i]
+        mean_x = sum_x/N
+        
+        
+    sum_x2 = 0
+    #computing the mean of squares
+    
+    for i in range(N):
+        sum_x2 = sum_x2 + x[i]**2
+        mean_x2 = sum_x2/N
+        
+            
+    #computing the variance
+    
+    variance = mean_x2 - (mean_x**2)
+    
+    
+    #compute std
+    
+    std = math.sqrt(variance)
+    
+    return std
+
+
+# Method 2
+
+def std_buildtin(x):
+    N = len(x)
+    
+    #compute sum and mean
+    
+    mean_y = sum(x)/N
+    
+    #compute mean of squares
+    
+    mean_y2 = sum(i**2 for i in x)/N
+    
+    
+    #Compute the variance
+    
+    variance_y = mean_y2 - (mean_y **2)
+    
+    #compute the standard deviation
+    
+    std_y = math.sqrt(variance_y)
+
+    return std_y
+
+# Method 3
+
+import numpy
+
+def std_numpy(x):
+    return numpy.std(x)
+
+print(std_loops(num_list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
